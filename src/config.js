e08427892619b9105d85448ad8934107d205be5a
Lin <linmaweb@gmail.com>
2020-09-30 02:38:16 +1000

    components updated

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@
 /***********************************************/
 
 const firebaseConfig = {
-  apiKey: "YOUR FIREBASE CONFIG HERE",
-  authDomain: "YOUR FIREBASE CONFIG HERE",
-  databaseURL: "YOUR FIREBASE CONFIG HERE",
-  projectId: "YOUR FIREBASE CONFIG HERE",
-  storageBucket: "YOUR FIREBASE CONFIG HERE",
-  messagingSenderId: "YOUR FIREBASE CONFIG HERE",
-  appId: "YOUR FIREBASE CONFIG HERE",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export default firebaseConfig;
